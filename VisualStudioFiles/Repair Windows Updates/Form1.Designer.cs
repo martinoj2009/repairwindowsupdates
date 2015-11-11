f972bfbf9dfaaec993da67e8135f69524bbd0369
Martino <martinojones@outlook.com>
2015-11-10 20:49:12 -0500

    Changed the UI around a bit

--- VisualStudioFiles/Repair Windows Updates/Form1.Designer.cs
+++ VisualStudioFiles/Repair Windows Updates/Form1.Designer.cs
@@ -42,18 +42,20 @@
             // statusLable
             // 
             this.statusLable.AutoSize = true;
-            this.statusLable.Location = new System.Drawing.Point(9, 51);
+            this.statusLable.Location = new System.Drawing.Point(14, 78);
+            this.statusLable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.statusLable.Name = "statusLable";
-            this.statusLable.Size = new System.Drawing.Size(43, 13);
+            this.statusLable.Size = new System.Drawing.Size(64, 20);
             this.statusLable.TabIndex = 0;
             this.statusLable.Text = "Status: ";
             // 
             // statusText
             // 
             this.statusText.AutoSize = true;
-            this.statusText.Location = new System.Drawing.Point(59, 51);
+            this.statusText.Location = new System.Drawing.Point(88, 78);
+            this.statusText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.statusText.Name = "statusText";
-            this.statusText.Size = new System.Drawing.Size(29, 13);
+            this.statusText.Size = new System.Drawing.Size(44, 20);
             this.statusText.TabIndex = 1;
             this.statusText.Text = "Start";
             // 
@@ -61,16 +63,19 @@
             // 
             this.completedList.FormattingEnabled = true;
             this.completedList.HorizontalScrollbar = true;
-            this.completedList.Location = new System.Drawing.Point(12, 178);
+            this.completedList.ItemHeight = 20;
+            this.completedList.Location = new System.Drawing.Point(13, 203);
+            this.completedList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.completedList.Name = "completedList";
-            this.completedList.Size = new System.Drawing.Size(294, 212);
+            this.completedList.Size = new System.Drawing.Size(451, 324);
             this.completedList.TabIndex = 2;
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(13, 149);
+            this.startButton.Location = new System.Drawing.Point(13, 158);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.Size = new System.Drawing.Size(112, 35);
             this.startButton.TabIndex = 3;
             this.startButton.Text = "Start Fix";
             this.startButton.UseVisualStyleBackColor = true;
@@ -78,11 +83,13 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(318, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(477, 35);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -91,23 +98,23 @@
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
             this.helpToolStripMenuItem.Text = "Help";
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 30);
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(318, 391);
+            this.ClientSize = new System.Drawing.Size(477, 545);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.completedList);
             this.Controls.Add(this.statusText);
@@ -116,6 +123,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
